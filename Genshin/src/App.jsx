48a638c3aef75c710acac841677133805f895257
Endrigogustavo <endrigogustavo2006@gmail.com>
2023-11-14 11:29:52 -0300

    m

---
+++ Genshin/src/App.jsx
@@ -0,0 +1,17 @@
+
+import Navbar from './Componentes/Navbar/Nav.jsx'
+import Car from './Componentes/Car/Car.jsx'
+import Body from './Componentes/Body/Body.jsx'
+function App() {
+ 
+
+  return (
+    <>
+   <Navbar/>
+   <Car/>
+    <Body/>
+    </>
+  )
+}
+
+export default App
